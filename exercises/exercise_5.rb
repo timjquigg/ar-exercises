e47b108725380d963c7523f3b1e59e0538fc0a74
Tim Quigg <timjquigg@gmail.com>
2022-12-12 18:44:37 -0700

    Complete exercises 5 & 6.

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,10 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+total_revenu = Store.sum(:annual_revenue)
+total_stores = Store.count
+puts "Total revenue: $#{total_revenu}"
+puts "Average revenue: $#{total_revenu / total_stores}"
+
+total_big_stores = Store.where('annual_revenue >= ?', 1000000).count
+puts "Total number of stores with $1M or more in annual revenue: #{total_big_stores}"
--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,8 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "Alison", last_name: "Wonderland", hourly_rate: 60)
+@store1.employees.create(first_name: "Bob", last_name: "Doe", hourly_rate: 60)
+@store2.employees.create(first_name: "Cindy", last_name: "Loo", hourly_rate: 60)
+@store2.employees.create(first_name: "Doug", last_name: "McKenzie", hourly_rate: 60)
